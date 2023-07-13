1358de9e34cab5c8a7ffcc3088e4b36d580e78ef
margo <margochin98@gmail.com>
2023-07-13 17:25:58 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  assetsPublicPath: './'
+publicPath:'./dist'
 })
 
 // publicPath: process.env.NODE_ENV === 'production'
-// ? '/dist/' 發佈網頁，生成首頁
+// ? '/dist/' 發佈網頁，生成
 // : '/'
